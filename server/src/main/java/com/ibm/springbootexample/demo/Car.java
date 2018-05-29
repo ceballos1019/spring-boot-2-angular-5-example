d5df4eeb0e776ffa178a9be87465c1899ac59e0f
Andres Ceballos Sanchez <andres.ceballos@ibm.com>
2018-05-29 15:47:45 -0500

    Added server and client modules

---
+++ server/src/main/java/com/ibm/springbootexample/demo/Car.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.ibm.springbootexample.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author andressanchez
+ *
+ */
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@ToString @EqualsAndHashCode
+public class Car {
+	
+	@Id @GeneratedValue
+	private Long id;
+	private @NonNull String name;
+}
---
+++ server/src/main/java/com/ibm/springbootexample/demo/CarRepository.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.ibm.springbootexample.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+/**
+ * @author andressanchez
+ *
+ */
+@RepositoryRestResource
+@CrossOrigin(origins = "http://localhost:4200")
+interface CarRepository extends JpaRepository<Car, Long>{
+
+}
